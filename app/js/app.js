76c7350457aa00c96f4b3b899747b3e48bdc81fc
Luis <luiswayne2530@gmail.com>
2026-09-02 20:48:06 -0400

    Connect dashboard to Zoho CRM SDK

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,27 @@
+// const status = document.getElementById("crm-status");
+
+// window.onload = () => {
+//   if (!window.ZOHO) {
+//     status.textContent = "Running locally...";
+//     return;
+//   }
+
+//   ZOHO.embeddedApp.on("PageLoad", function (data) {
+//     console.log(data);
+
+//     status.textContent = "Connected to Zoho CRM ✅";
+//   });
+
+//   ZOHO.embeddedApp.init();
+// };
+// document.getElementById("searchBtn").addEventListener("click", () => {
+//   const searchText = document.getElementById("searchInput").value;
+
+//   console.log("Searching:", searchText);
+
+//   alert(`Searching for: ${searchText}`);
+// });
+
 const status = document.getElementById("crm-status");
 
 window.onload = () => {
@@ -6,11 +30,15 @@ window.onload = () => {
     return;
   }
 
-  ZOHO.embeddedApp.on("PageLoad", function (data) {
-    console.log(data);
-
+  ZOHO.embeddedApp.on("PageLoad", () => {
     status.textContent = "Connected to Zoho CRM ✅";
   });
 
   ZOHO.embeddedApp.init();
 };
+
+document.getElementById("searchBtn").addEventListener("click", () => {
+  const searchText = document.getElementById("searchInput").value;
+
+  alert(`Searching for: ${searchText}`);
+});
